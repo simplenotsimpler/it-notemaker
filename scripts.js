28e7664eb95d4dfc2944a20a04c4e27b4375aaec
SimpleNotSimpler <simplenotsimpler@outlook.com>
2024-09-30 06:04:40 -0400

    start fetch from json file

--- scripts.js
+++ scripts.js
@@ -1,5 +1,19 @@
 // console.log("hello from script");
 
+// https://www.geeksforgeeks.org/read-json-file-using-javascript/
+function fetchJSONData() {
+  fetch("./db.json")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
+
 // https://www.youtube.com/watch?v=18kROgRlRDQ
 const osList = document.getElementById("os-list");
 // TODO: fetch this from db.json
@@ -13,3 +27,16 @@ operatingSystems.forEach((os) => {
   option.textContent = os.name;
   osList.appendChild(option);
 });
+
+function fetchJSONData() {
+  fetch("./db.json")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
